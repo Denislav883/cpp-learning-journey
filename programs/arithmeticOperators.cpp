4649e88b39c24b62841a61f456e4fc02fc404cee
Denislav883 <yanev.denislav@gmail.com>
2026-01-20 20:58:46 +0200

    Add arithmeticOperators.cpp example
    Introduces a new C++ program demonstrating basic arithmetic operators and their shorthand forms using a 'students' variable.

---
+++ programs/arithmeticOperators.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+    int students = 30;
+
+    // students = students + 1; // students += 1; // students++ 
+
+    // students = students - 1; // students -= 1; // students--
+
+    // students = students * 5;
+    students *= 5;
+
+    // students = students / 2; // students /= 2; //students = students % 2 (% symbol - modular division, returns the reminder of the equation)
+
+    std::cout << students;
+}
