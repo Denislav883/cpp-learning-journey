d1dff72ae84dccfba9099b9914d3e9c7861adbf3
Denislav883 <yanev.denislav@gmail.com>
2026-01-25 22:43:24 +0200

    Add while loop example to prompt for user name
    Introduces a new C++ program that uses a while loop to repeatedly prompt the user to enter their name until a non-empty value is provided.

---
+++ programs/whileLoop.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string name;
+
+    while(name.empty()) {
+        cout << "Enter your name: ";
+        getline(cin, name);
+    }
+
+    cout << "Your name is " << name;
+
+    return 0;
+}
