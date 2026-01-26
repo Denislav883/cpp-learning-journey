f3772e30b8420fe40f90d01d4e394b6f11646d2f
Denislav883 <yanev.denislav@gmail.com>
2026-01-26 21:56:18 +0200

    Fix typo in do-while loop comment

--- programs/doWhileLoop.cpp
+++ programs/doWhileLoop.cpp
@@ -7,7 +7,7 @@ int main() {
     do {
         cout << "Enter a positive number: ";
         cin >> number;
-    } while(number < 0); // whis will first do the code once and then will continue if the condition is true
+    } while(number < 0); // this will first execute the block code once and then will continue executing the code again if the condition is true
 
     cout << "Your number is: " << number;
     return 0;
