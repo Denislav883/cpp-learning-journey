c59e9f64372dba515525853cbfb7506dd151483c
Denislav883 <yanev.denislav@gmail.com>
2026-01-26 22:00:00 +0200

    Add for loop example in C++
    Introduces a simple C++ program demonstrating a for loop that prints numbers from 1 to 3 and a concluding message.

---
+++ programs/forLoop.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    for(int i = 1; i <= 3; i++) {
+        cout << "The current number you get is: " << i << endl;
+    }
+
+    cout << "These was all the numbers you got!";
+    return 0;
+}
