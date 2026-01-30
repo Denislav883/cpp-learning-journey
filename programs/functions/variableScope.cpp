e94b5306aeb420c1f0ef7253ad39b17d98ec380f
Denislav883 <yanev.denislav@gmail.com>
2026-01-30 16:51:11 +0200

    Add example for variable scope in C++
    Introduces variableScope.cpp demonstrating local and global variable scope, including use of the scope resolution operator (::) and function-level variable shadowing.

---
+++ programs/functions/variableScope.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+    int myNum = 3;
+
+    void printNum();
+
+int main() {
+    int myNum = 1;
+
+    cout << myNum << "\n";
+    printNum();
+    cout << ::myNum << "\n"; // :: this will ignore the local scope and will print myNum from the global scope
+    
+    return 0;
+}
+
+void printNum() {
+    int myNum = 2;
+    cout << myNum << "\n";
+}
