df62548195ad2c064e8b29b39b455c927b5d1348
Denislav883 <yanev.denislav@gmail.com>
2026-01-30 20:31:16 +0200

    Add array example to print prices in C++
    Introduces a new C++ file demonstrating array usage by printing elements of a double array named 'prices'.

---
+++ exercises/arrays/array.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double prices[] = {5.00, 7.00, 9.50, 13.00};
+
+    cout << prices[0] << "\n";
+    cout << prices[1] << "\n";
+    cout << prices[2] << "\n";
+    cout << prices[3] << "\n";
+
+    return 0;
+}
